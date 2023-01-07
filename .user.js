7e3da32baf9d7fc07558b6cbf7685ab8e849d0d2
LALOL <89741263+Its-LALOL@users.noreply.github.com>
2023-01-07 14:46:24 +0300

    Create .user.js

---
+++ .user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         ⚡ LALOL Cord
+// @description  Discord flooder, webhook deleter
+// @version      ㅤ
+// @author       LALOL
+// @match        *://*.discord.com/*
+// @icon         https://canary.discord.com/assets/ec2c34cadd4b5f4594415127380a85e6.ico
+// @grant        none
+// ==/UserScript==
+
+print('test')
