ff6fc87e53f524193e3af648d6a96696b29c67b3
LALOL <89741263+Its-LALOL@users.noreply.github.com>
2023-01-08 13:30:23 +0300

    Update a.user.js

--- a.user.js
+++ a.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ⚡ LALOL Cord
 // @namespace    https://github.com/Its-LALOL/LALOL-Cord
-// @description  Discord flooder, webhook deleter
+// @description  Discord flooder, webhook deleter, login using token
 // @version      ㅤ
 // @author       LALOL
 // @match        *://*.discord.com/*
