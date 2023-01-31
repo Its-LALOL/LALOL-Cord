2db636fe746fb68a6c96d525784dbc5b307a31a4
LALOL <89741263+Its-LALOL@users.noreply.github.com>
2023-01-31 18:47:48 +0300

    Update a.user.js

--- a.user.js
+++ a.user.js
@@ -7,6 +7,9 @@
 // @match        *://*.discord.com/*
 // @icon         https://github.com/Its-LALOL/LALOL-Cord/raw/main/icon.png
 // @grant        none
+// @supportURL   https://github.com/Its-LALOL/LALOL-Cord/issues
+// @updateURL    https://github.com/Its-LALOL/LALOL-Cord/raw/main/source.js
+// @downloadURL  https://github.com/Its-LALOL/LALOL-Cord/raw/main/source.js
 // @require      https://github.com/Its-LALOL/LALOL-Cord/raw/main/source.js
 // ==/UserScript==
 
